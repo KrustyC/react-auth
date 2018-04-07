92fb6b5c55ec0dcb32ae14bc6ccecb83e1261ea5
Davide <davide.crestini94@gmail.com>
2018-04-07 19:56:17 +0100

    minor tweaks on headings

--- src/uikit/docs/index.jsx
+++ src/uikit/docs/index.jsx
@@ -1,3 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default () => "Docs"
+import { Title } from '../'
+
+const Div = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export default () => (
+  <Div>
+    <Title>This page will contain the documentation for the uikit</Title>
+  </Div>
+)
--- src/uikit/elements/Text.jsx
+++ src/uikit/elements/Text.jsx
@@ -4,8 +4,12 @@ const P = styled.p`
   font-size: ${({ theme }) => theme.fontSize};
 `
 
-const Title = P.withComponent('h1')
+const Title = P.withComponent('h1').extend`
+  font-size: 24px;
+`
 
-const Subtitle = P.withComponent('h2')
+const Subtitle = P.withComponent('h2').extend`
+  font-size: 24px;
+`
 
 export { P, Title, Subtitle }
